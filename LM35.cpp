57e1410eb392c57acf665351932ac1bd92919531
SAM MICHAEL S <153702953+sms32@users.noreply.github.com>
2024-02-17 22:04:31 +0530

    Update LM35.cpp

--- LM35.cpp
+++ LM35.cpp
@@ -3,7 +3,7 @@ int temp = A1 ;
 void setup()
 {
   pinMode(temp,INPUT);
-pinMode(12,output);
+pinMode(12,OUTPUT);
 Serial.begin(9600);
 }
 
@@ -12,12 +12,14 @@ void loop()
   int LM35 = analogRead(temp);
 int celsius = (LM35 * 500)/1024;
 Serial.print(celsius);
+delay(1000);
+Serial.print("\n");
 if (celsius >= 25)
 {
 digitalWrite(12,HIGH);
 }
 else
 {
-digitalWrite(12,LOW)
+digitalWrite(12,LOW);
   }
 }
